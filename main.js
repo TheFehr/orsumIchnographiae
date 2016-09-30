66d1b3c464661e4cb704813b897c9ff0d729dbad
kbwschüler <kbwschuler@medio-02.local>
2016-09-30 10:20:35 +0200

    map mit Drag

--- main.js
+++ main.js
@@ -3,5 +3,12 @@
 
     window.addEventListener("load", function(loadEvent) {
 
+        var map = document.querySelector("#map");
+        map.addEventListener("click", function(e) {
+            var img = map.getElementsByTagName("img")[0];
+            img.width = 5712;
+            img.height = 2986;
+            img.style.height = "auto";
+        });
     });
 })();
