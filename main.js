b1b98415c545f99ef6fb355497189844b9b0f720
kbwschüler <kbwschuler@medio-02.local>
2016-09-30 10:36:00 +0200

    map object & Zoom out

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 (function() {
     "use strict";
 
-    window.addEventListener("load", function(loadEvent) {
+    var map;
 
-        var map = document.querySelector("#map");
-        map.addEventListener("click", function(e) {
-            var img = map.getElementsByTagName("img")[0];
-            img.width = 5712;
-            img.height = 2986;
-            img.style.height = "auto";
+    function initMap() {
+        map = {
+            html: document.querySelector("#map"),
+            img: document.querySelector("#map").getElementsByTagName("img")[0],
+            zoomedIn: false,
+
+            zoomMapIn: function zoomMapIn() {
+                map.img.width = 5712;
+                map.img.height = 2986;
+                map.img.style.height = "auto";
+                map.html.style.cursor = "zoom-out";
+                map.zoomedIn = true;
+            },
+
+            zoomMapOut: function() {
+                map.img.removeAttribute("width");
+                map.img.removeAttribute("height");
+                map.img.style.height = "100%";
+                map.html.style.cursor = "zoom-in";
+                map.zoomedIn = false;
+            }
+        };
+    }
+
+    window.addEventListener("load", function(loadEvent) {
+        initMap();
+
+        map.html.addEventListener("click", function(e) {
+            if (!map.zoomedIn)
+                map.zoomMapIn();
+            else
+                map.zoomMapOut();
         });
     });
 })();
