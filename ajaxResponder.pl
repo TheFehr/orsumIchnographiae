8496498b115ed8fc6263ae2293328caf2b286105
Philipp Fehr <calvin.philipp@gmail.com>
2016-11-18 10:26:30 +0100

    Update 3, nix

--- ajaxResponder.pl
+++ ajaxResponder.pl
@@ -3,5 +3,3 @@ use v5.10.0;
 use warnings;
 use strict;
 
-print "Content-type: text/html\n\n";
-print "<html><body><h1>huhu</h1></body></html>";
--- databankHandler.pl
+++ databankHandler.pl
@@ -6,7 +6,7 @@ use strict;
 package DatabaseHandler;
 sub new
 {
-  
+
 
 
 }
