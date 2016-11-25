20732f9a2737075e44887fa5e7509b61f823cf86
Lukas Bischof <polinderis@gmail.com>
2016-11-25 10:23:42 +0100

    basic database communication

--- ajaxResponder.pl
+++ ajaxResponder.pl
@@ -1,7 +1,36 @@
-#!/usr/bin/env/perl
+#!/usr/bin/perl
 use v5.10.0;
 use warnings;
-use strict;
+use DBI;
+use diagnostics;
+use CGI;
 
-print "Content-type: text/html\n\n";
-print "<html><body><h1>huhu</h1></body></html>";
+print "Content-Type: text/html\n\n";
+
+sub selectEvents {
+	my $db = shift;
+	my $stmt = $db->prepare('SELECT * FROM events');
+	$stmt->execute() or die $stmt->err_str;
+	
+	return $stmt;
+}
+
+
+my ($db_user, $db_name, $db_pass) = ('3i', 'orsumIchnographiae', 'wurst');
+my $db = DBI->connect("DBI:mysql:database=$db_name", $db_user, $db_pass);
+
+$statement = selectEvents($db);
+
+print "<html><head></head><body>";
+while (my ($col_1, $col_2, $col_3, $col_4) = $statement->fetchrow_array() ) {
+    print "<ul><li>Spalte 1: $col_1 \n</li>";
+    print "<li>Spalte 2: $col_2 \n</li>";
+    print "<li>Spalte 3: $col_3 \n</li>";
+    print "<li>Spalte 4: $col_4 \n</li></ul>";
+}
+
+my $cgi = CGI->new();
+my $param = $q->param('buh');
+print($param);
+
+print "</body></html>";
